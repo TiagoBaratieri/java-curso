38b5e445485e99abb187f6cf03f5601297b0b1ec
Tiago Baratieri <tiagobaratieri97@gmail.com>
2022-03-10 13:01:18 -0300

    FileReader e BufferedReader

---
+++ java-file/exemplo-file-java2/src/aplication/Program.java
@@ -0,0 +1,37 @@
+package aplication;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        String path = "c:\\temp\\text.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (fr != null && br != null) {
+                    br.close();
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

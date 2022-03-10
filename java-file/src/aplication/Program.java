01288a27190fdc5bb7cb64b2ecc820e2905404fd
Tiago Baratieri <tiagobaratieri97@gmail.com>
2022-03-10 12:29:32 -0300

    Leitura basica de arquivo

---
+++ java-file/src/aplication/Program.java
@@ -0,0 +1,25 @@
+package aplication;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        File file = new File("c:\\temp\\text.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNext()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null)
+            sc.close();
+        }
+    }
+}
